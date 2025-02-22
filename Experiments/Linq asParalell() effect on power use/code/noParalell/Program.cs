074b27f80f3e431829475cc413d46efa6d92cb78
Kent Inge Fagerland Simonsen <40064+kentis@users.noreply.github.com>
2025-02-22 12:50:48 +0100

    draft og asParalell CPU time article (#3)

---
+++ Experiments/Linq asParalell() effect on power use/code/noParalell/Program.cs
@@ -0,0 +1,25 @@
+namespace Program {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Enumerable.Range(1, 1_000_000_000).Select(x=> (long)x).ToList<long>();
+
+            Console.WriteLine($"{numbers.Sum()}");
+            Console.WriteLine($"{numbers.Sum()}");
+            Console.WriteLine($"{numbers.Sum()}");
+            Console.WriteLine($"{numbers.Sum()}");
+            Console.WriteLine($"{numbers.Sum()}");
+            Console.WriteLine($"{numbers.Sum()}");
+            Console.WriteLine($"{numbers.Sum()}");
+            Console.WriteLine($"{numbers.Sum()}");
+            Console.WriteLine($"{numbers.Sum()}");
+            Console.WriteLine($"{numbers.Sum()}");
+        }
+    }
+}
